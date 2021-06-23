feb8a166360f19d5be85cdf7f8b20bf9606b3dba
Jonathan Sauter (Contractor) <jonathansauter@sjrb.ad>
2021-06-23 11:00:16 -0600

    Initial Commit

--- lib/bigcdktest-stack.ts
+++ lib/bigcdktest-stack.ts
@@ -1,9 +1,53 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as asg from '@aws-cdk/aws-autoscaling';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import initMetadata = require('@shaw-cdk/ec2service');
+import path = require('path');
+import { AssetStaging } from '@aws-cdk/core';
+
 
 export class BigcdktestStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, 'MyBucket', {
+      bucketName: 'jsauter-my-bucket',
+      versioned: true
+    });
+
+    const vpc = ec2.Vpc.fromLookup(this, 'myVpc', {
+      vpcId: 'vpc-894b89ef'
+    });
+
+    const autoscalinggroup = new asg.AutoScalingGroup(this, 'ASG', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: new ec2.AmazonLinuxImage(),
+    });
+
+
+    const builder = new initMetadata.CfnInitMetadataBuilder(autoscalinggroup, 'configset1')
+
+    for (let i = 0; i < 10; i++) {
+      const file = new initMetadata.CfnInitScriptAsset(this, `${i}testScript`, {
+        friendlyName: `${i}-testScript`,
+        shouldExecute: false,
+        destinationPath: '\\temp\\scripts\\',
+        destinationFileName: `${i}testScript`,
+        path: path.join(__dirname, '../scripts/testScript.sh'),
+        env: {
+          ITERATION: `${i}`
+        },
+      });
+  
+      builder
+        .withScript(file)  
+    }
+
+    
+    
+    builder.build();
+
   }
 }
---
+++ scripts/testScript.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -ex
+
+echo "file iteration ${ITERATION}"
